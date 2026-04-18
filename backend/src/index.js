bc846adeec0f23171fee63ca753b5a49ec54395a
Santhoshkumar Rajeshkannan <sandy@Santhoshkumars-MacBook-Air.local>
2026-04-17 22:28:49 -0700

    Updates cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://smartdiet-cdss.vercel.app'
   ],
   credentials: true
 }))
